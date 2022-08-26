381924a8d453cc9216f316cf58d0f6f8cd933887
Jan Marius <Jan.Marius@bitbucket.no>
2022-08-26 08:41:07 +0200

    The Tibber SDK does not seem to like restarting the realtime listeners so this is a workaround.

--- src/Infrastructure/TibberClient.cs
+++ src/Infrastructure/TibberClient.cs
@@ -7,23 +7,23 @@ namespace tibberservice.Infrastructure;
 public class TibberClient
 {
     private readonly TibberConfig _config;
-    private readonly TibberApiClient _apiClient;
 
     public TibberClient(IOptions<TibberConfig> config)
     {
         config.Value.ThrowIfNotValid();
         _config = config.Value;
-        _apiClient = new TibberApiClient(_config.Key, _config.GetHeader());
     }
 
-    public async Task<IObservable<RealTimeMeasurement>> StartListener(Guid homeId, CancellationToken stopToken) =>
-        await _apiClient.StartRealTimeMeasurementListener(homeId, stopToken);
-
-    public async Task StopListener(Guid homeId) => await _apiClient.StopRealTimeMeasurementListener(homeId);
+    public async Task<IObservable<RealTimeMeasurement>> StartListener(Guid homeId, CancellationToken stopToken)
+    {
+        var apiClient = new TibberApiClient(_config.Key, _config.GetHeader());
+        return await apiClient.StartRealTimeMeasurementListener(homeId, stopToken);
+    }
     
     public async Task<IEnumerable<Home>> GetRealTimeConsumptionHomes()
     {
-        var basicData = await _apiClient.GetBasicData();
+        var apiClient = new TibberApiClient(_config.Key, _config.GetHeader());
+        var basicData = await apiClient.GetBasicData();
         return basicData
             .Data
             .Viewer
